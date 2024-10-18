fa00c2d61a02638ebc9309b616662f64f1983209
XeRxEs02 <158118278+XeRxEs02@users.noreply.github.com>
2024-10-18 22:16:53 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT=5555;
+
+export const mongoDBURL='mongodb+srv://Book_store:mern123@book-store.8jjz7.mongodb.net/Book-Store?retryWrites=true&w=majority&appName=Book-Store'
---
+++ frontend/home/BookCard.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import { PiBookOpenTextLight } from 'react-icons/pi';
+import { BiUserCircle } from 'react-icons/bi';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineDelete } from 'react-icons/md';
+import BookSingleCard from './BookSingleCard.jsx';
+import BackButton from '../src/components/BackButton.jsx';
+
+const BookCard = ({ books }) => {
+  return (
+    <div className='p-4'>
+    <BackButton/>
+      <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4' >
+
+        {books.map((item) => (
+          <BookSingleCard key={item._id} book={item} />
+        ))}
+
+      </div>
+    </div>
+  );
+};
+
+export default BookCard;
